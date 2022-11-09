8682bacc57aaa0282503913bfa601281258e774d
Jinyu <cadenho@hotmail.com>
2022-11-09 19:42:09 +0800

    feat: init

---
+++ lib/main.dart
@@ -0,0 +1,96 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          ///
+        },
+        child: const Icon(Icons.add),
+      ),
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: WebView(
+        initialUrl: Uri.dataFromString('''
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <style>
+    * {
+      margin: 0;
+    }
+  </style>
+</head>
+<body>
+  <h1 id="h1" style="position: fixed; height: 100vh; width: 100vw; background-color: red;">
+  press FloatingActionButton you can see the touch event fired
+  </h1>
+  <script>
+    const el = document.getElementById('h1')
+    el.ontouchstart = () => {
+      el.innerText = 'touched!!!' + new Date().toString()
+    }
+  </script>
+</body>
+</html>
+                ''', mimeType: 'text/html').toString(),
+        javascriptMode: JavascriptMode.unrestricted,
+      ),
+// This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
